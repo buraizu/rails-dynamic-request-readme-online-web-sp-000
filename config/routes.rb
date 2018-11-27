79cf0c078d4e3eda48ebeb87d743e7d372ebe5ed
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-27 07:42:00 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   # get 'posts/:id', to: 'posts#show'    #Longer style
   resources :posts, only: :show
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -57,5 +57,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  
+
 end
